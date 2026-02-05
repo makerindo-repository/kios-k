af01c2b7ac59f841b0a8f0a6da146a8de1f7bb48
valiant77 <achmadfahri0721@gmail.com>
2026-02-05 11:59:24 +0700

    adding content, remove mockup, dynamic clock

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+//for changing pages
 const pages = document.querySelectorAll(".page");
-const INTERVAL = 5_000; // 30 seconds
+const INTERVAL = 7_000;
 let index = 0;
 
 setInterval(() => {
@@ -7,3 +8,33 @@ setInterval(() => {
     index = (index + 1) % pages.length;
     pages[index].classList.add("active");
 }, INTERVAL);
+
+//for date and clock
+const timeEl = document.getElementById("time");
+const dateEl = document.getElementById("date");
+
+const days = ["Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu"];
+const months = [
+  "Januari","Februari","Maret","April","Mei","Juni",
+  "Juli","Agustus","September","Oktober","November","Desember"
+];
+
+function updateClock() {
+  const now = new Date();
+
+  //time
+  const hours = now.getHours().toString().padStart(2, "0");
+  const minutes = now.getMinutes().toString().padStart(2, "0");
+  const seconds = now.getSeconds().toString().padStart(2, "0");
+  timeEl.textContent = `${hours}:${minutes}:${seconds}`;
+
+  //date
+  const dayName = days[now.getDay()];
+  const date = now.getDate();
+  const monthName = months[now.getMonth()];
+  const year = now.getFullYear();
+  dateEl.textContent = `${dayName}, ${date} ${monthName} ${year}`;
+}
+
+updateClock();
+setInterval(updateClock, 1000);
