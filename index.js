c096d1c2d7b325b3024c74abe1ac90fbbb074a89
valiant77 <achmadfahri0721@gmail.com>
2026-02-09 15:58:47 +0700

    adding more contents

--- index.js
+++ index.js
@@ -36,22 +36,22 @@ function updateClock() {
   const year = now.getFullYear();
   dateEl.textContent = `${dayName}, ${date} ${monthName} ${year}`;
 
-  if (hours >= "6" && hours < "12") {
+  if (hours >= 6 && hours < 12) {
     message.textContent = "Selamat Pagi!"
-    if (hours === "8" && minutes === "0") {
+    if (hours == 9 && minutes == 22) {
       message.textContent = "Selamat Bekerja!"
     }
   }
-  else if (hours >= "12" && hours < "15") {
-    if (hours === "12" && minutes === "0") {
+  else if (hours >= 12 && hours < 15) {
+    if (hours == 12 && minutes == 0) {
       message.textContent = "Selamat Makan Siang, Semuanya!"
     }
     message.textContent = "Selamat Siang!"
   }
-  else if (hours >= "15" && hours < "18") {
+  else if (hours >= 15 && hours < 18) {
     message.textContent = "Selamat Sore!"
-    if (hours === "17" && minutes === "0") {
-      if (dayName === "Jumat") {
+    if (hours == 17 && minutes == 0) {
+      if (dayName == "Jumat") {
         message.textContent = "Sampai Jumpa Senin Nanti!"
       }
       else {
