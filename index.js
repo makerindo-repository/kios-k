7fac1ab4ee3e2347d99971dfad5db393185f6fae
valiant77 <achmadfahri0721@gmail.com>
2026-02-10 16:40:00 +0700

    MoU change, fixing structure, and add activity

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //for changing pages
 const pages = document.querySelectorAll(".page");
-const INTERVAL = 7_000;
+const INTERVAL = 7_300;
 let index = 0;
 
 setInterval(() => {
@@ -8,6 +8,64 @@ setInterval(() => {
     index = (index + 1) % pages.length;
     pages[index].classList.add("active");
 }, INTERVAL);
+
+//for the activity page changing
+const activities = [
+  {
+    title: "Ujikom Prodi Teknologi Komputer",
+    description: "Uji kompetensi ini dilaksanakan dari 22 Desember sampai 24 Desember 2025 di Politeknik Pajajaran. Bidang kompetensinya di antara lain: Junior Web Programmer / Developer, dan Basic Industrial Internet of Things.",
+    images: [
+      "assets/activities/ujikom1.jpg",
+      "assets/activities/ujikom2.png",
+      "assets/activities/ujikom3.png"
+    ]
+  },
+  {
+    title: "Ujikom SMK Negeri 1 Katapang",
+    description: "Dilaksanakan pada tanggal 9 Desember 2025 di SMK Negeri 1 Katapang. Diikuti oleh seluruh kelas 12 / XII jurusan Rekayasa Perangkat Lunak.",
+    images: [
+      "assets/activities/katapang1.jpg",
+      "assets/activities/katapang2.jpg",
+      "assets/activities/katapang3.jpg"
+    ]
+  }
+];
+const page = document.querySelector(".activity-page")
+const sliderImg = page.querySelector(".activity-slider img");
+const titleEl = page.querySelector("#activity-title");
+const descEl = page.querySelector("#activity-desc");
+
+let activityIndex = 0;
+let imageIndex = 0;
+let imageInterval = null;
+
+function startActivity(activity) {
+  titleEl.textContent = activity.title;
+  descEl.textContent = activity.description;
+
+  imageIndex = 0;
+  sliderImg.src = activity.images[0];
+
+  if (imageInterval) {
+    clearInterval(imageInterval);
+  }
+
+  imageInterval = setInterval(() => {
+    imageIndex = (imageIndex + 1) % activity.images.length;
+    sliderImg.src = activity.images[imageIndex];
+  }, 2500);
+}
+
+function startActivityRotation() {
+  startActivity(activities[activityIndex]);
+
+  setInterval(() => {
+    activityIndex = (activityIndex + 1) % activities.length;
+    startActivity(activities[activityIndex]);
+  }, 7500)
+}
+
+startActivityRotation();
 
 //for date and clock
 const timeEl = document.getElementById("time");
