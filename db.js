818dc51ca69bac9de52f068938bf0aae4e7a42cd
valiant77 <achmadfahri0721@gmail.com>
2026-03-31 15:47:12 +0700

    database change

--- db.js
+++ db.js
@@ -2,9 +2,9 @@
 const mysql = require("mysql2/promise");
 const pool = mysql.createPool({
     host: "localhost",
-    user: "root",
-    password: "Valiant77",
-    database: "kiosk2026",
+    user: "kiosk",
+    password: "PASSWORD",
+    database: "kiosk",
     waitForConnections: true,
     connectionLimit: 10
 });
